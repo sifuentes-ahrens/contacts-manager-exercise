69ad9b82533158277d2aee0b1f5ccbe8746cae02
aaronsifuentes47 <sifuentes.aaronm@gmail.com>
2019-08-09 14:07:08 -0500

    final progress

--- src/Input.java
+++ src/Input.java
@@ -10,27 +10,22 @@ import java.util.Scanner;
 
 public class Input {
     private Scanner scan = new Scanner(System.in);
-    //view contact list
-
-//    public static void main(String[] args) {
-//        ArrayList<String> contactList = new ArrayList<>();
-//      readFile(contactList);
-//    }
     public void viewContact() {
         // #1 view contact list
-        try {
-            List<String> contactList = new ArrayList<>();
-
-            Path contactsPath = Paths.get("data", "contacts.txt");
-            contactList = Files.readAllLines(contactsPath);
-            System.out.println("Name            |  Phone Number\n-----------------------");
-            for (int i = 0; i < contactList.size(); i += 1) {
-
-                System.out.println(contactList.get(i));
+        do {
+            System.out.println("Showing contacts... \n");
+            try {
+                List<String> contactList;
+                Path contactsPath = Paths.get("data", "contacts.txt");
+                contactList = Files.readAllLines(contactsPath);
+                System.out.println("Name        |  Phone Number  |\n--------------------------------");
+                for (String contact : contactList) {
+                    System.out.println(contact);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        } while (yesNo());
     }
     public void searchContacts() {
         // #3 search by name
@@ -45,18 +40,14 @@ public class Input {
             }
         } catch (IOException e) {
             System.out.println("Another Error! continuing...");
-
         }
-
     }
-
-    public void addContacts() {
+    public void addContactsAction() {
         //#2 add contacts
         Scanner scan = new Scanner(System.in);
         Input stuff = new Input();
-        Path contactsPath = Paths.get("data", "contacts.txt");
         do {
-            System.out.println("Enter your name: ");
+            System.out.println("Enter your first and last name: ");
             String name = scan.nextLine();
             String[] fullName = name.split(" ");
             System.out.println("Enter your Phone Number: ");
@@ -64,26 +55,19 @@ public class Input {
             try {
                 stuff.addContact(fullName[0], fullName[1], num);
             } catch (ArrayIndexOutOfBoundsException e) {
-                System.out.println("Incorrect format. Try again");
+                System.out.println("Incorrect format. Try again.");
                 continue;
             }
-                System.out.println("User Added! Continuing...");
-
-        } while (stuff.yesNo());
+            System.out.println("adding \"" + name + " " + num + "\" to database...");
+            System.out.println("User Added! Continuing...");
+            break;
+        } while (true);
     }
-
     public boolean yesNo() {
-        System.out.println("\ncontinue? [y/n]");
+        System.out.println("\nPress \"n\" to continue...");
         String answer = scan.nextLine().toLowerCase();
         return (answer.equalsIgnoreCase("y") || answer.contains("yes"));
     }
-    public boolean yesNoDelete() {
-        System.out.println("\ncontinue? [y/n]");
-        String answer = scan.nextLine().toLowerCase();
-        return (answer.equalsIgnoreCase("y") || answer.contains("yes"));
-    }
-
-
     public void addContact(String first, String last, String num) {
         //Add contacts
         Path dataFile = Paths.get("data", "contacts.txt");
@@ -101,24 +85,22 @@ public class Input {
         // #4 Delete by name
         System.out.println("Enter name to delete: ");
         String search = scan.nextLine().toLowerCase();
-        try {
-            List<String> lines = Files.readAllLines(Paths.get("data", "contacts.txt"));
-            for (String line : lines) {
-                if (line.toLowerCase().contains(search)) {
-                    System.out.println(line);
-                    lines.remove(line);
-                    viewContact();
+        boolean i = true;
+        do {
+            try {
+                List<String> lines = Files.readAllLines(Paths.get("data", "contacts.txt"));
+                List<String> newList = new ArrayList<>();
+                for (String line : lines) {
+                    if (!line.toLowerCase().contains(search)) {
+                        newList.add(line);
+                    }
+                    Files.write(Paths.get("data", "contacts.txt"), newList);
                 }
+                i = false;
+                System.out.println("Contact deleted. Returning to menu...");
+            } catch (IOException e) {
+                System.out.println("Another Error! continuing...");
             }
-        } catch (IOException e) {
-            System.out.println("Another Error! continuing...");
-
-        }
-//        List list = new ArrayList();
-//
-//        String element = "first element";
-//        list.add(element);
-//
-//        list.remove(element);
+        } while (i);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
+//import java.io.IOException;
+//import java.nio.file.Files;
+//import java.nio.file.Path;
+//import java.nio.file.Paths;
+//import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -10,7 +10,12 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         Input yesno = new Input();
 
+        boolean i = true;
+
        do {
+           System.out.println();
+           System.out.println("Welcome!");
+           System.out.println("-----------------------------------");
            System.out.println("1. View Contacts.\n2. Add a new contact.\n3. Search a contact by name.\n4. Delete an existing contact.\n5. Exit.");
            System.out.println("Enter an option (1, 2, 3, 4 or 5):");
         int answer = sc.nextInt();
@@ -20,7 +25,7 @@ public class Main {
                    yesno.viewContact();
                    break;
                case 2:
-                   yesno.addContacts();
+                   yesno.addContactsAction();
                    break;
                case 3:
                    yesno.searchContacts();
@@ -30,8 +35,9 @@ public class Main {
                    break;
                default:
                    System.out.println("Goodbye.");
+                   i = false;
                    break;
            }
-       } while (yesno.yesNo());
+       } while (i);
     }
 }
